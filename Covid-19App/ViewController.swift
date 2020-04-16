98f1603116f9087512453126eb91eca4e664ac61
ambeche <tamanji.che@metropolia.fi>
2020-04-16 11:48:43 +0300

    Initial Commit

---
+++ Covid-19App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19App
+//
+//  Created by iosdev on 16.4.2020.
+//  Copyright © 2020 Covid-19App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
