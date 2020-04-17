c798d945d954bf18a0c0bc8d229337e474478bb4
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2020-04-17 09:38:01 +0300

    second commit

--- Covid-19App/ViewController.swift
+++ Covid-19App/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         // comments from Rafe
+        // second comment from Rafe
     }
 
 
