e8c1048cbf4c733a277932997be7107709e9588f
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2020-04-29 11:11:45 +0300

    linked symptoms storyboard to main storyboard

--- Covid-19App/SymptomsViewController.swift
+++ Covid-19App/SymptomsViewController.swift
@@ -15,29 +15,29 @@ class SymptomsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    class CheckBox: UIButton {
-        let checkedImage = UIImage(contentsOfFile: "checked_checkbox")! as UIImage
-        let uncheckedImage = UIImage(contentsOfFile: "unchecked_checkbox")! as UIImage
-        
-        var isChecked: Bool = false {
-            didSet {
-                if isChecked == true {
-                    self.setImage(checkedImage, for: UIControl.State.normal)
-                } else {
-                    self.setImage(uncheckedImage, for: UIControl.State.normal)
-
-                }
-            }
-        }
-        override func awakeFromNib() {
-            self.addTarget(self, action: #selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
-            self.isChecked = false
-        }
-        @objc func buttonClicked(sender: UIButton) {
-            if sender == self{
-                isChecked = !isChecked
-            }}
-        }
+//    class CheckBox: UIButton {
+//        let checkedImage = UIImage(contentsOfFile: "checked_checkbox")! as UIImage
+//        let uncheckedImage = UIImage(contentsOfFile: "unchecked_checkbox")! as UIImage
+//        
+//        var isChecked: Bool = false {
+//            didSet {
+//                if isChecked == true {
+//                    self.setImage(checkedImage, for: UIControl.State.normal)
+//                } else {
+//                    self.setImage(uncheckedImage, for: UIControl.State.normal)
+//
+//                }
+//            }
+//        }
+//        override func awakeFromNib() {
+//            self.addTarget(self, action: #selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
+//            self.isChecked = false
+//        }
+//        @objc func buttonClicked(sender: UIButton) {
+//            if sender == self{
+//                isChecked = !isChecked
+//            }}
+//        }
     }
 
     /*
