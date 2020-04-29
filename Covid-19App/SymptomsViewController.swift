2d4dca2af79cf6bcf3c3bd59d72451dd72cf97bc
OulaJ <oula.jokela@metropolia.fi>
2020-04-29 10:51:39 +0300

    symptomsscreen

---
+++ Covid-19App/SymptomsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SymptomsViewController.swift
+//  Covid-19App
+//
+//  Created by iosdev on 29.4.2020.
+//  Copyright © 2020 Covid-19App. All rights reserved.
+//
+
+import UIKit
+
+class SymptomsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    class CheckBox: UIButton {
+        let checkedImage = UIImage(contentsOfFile: "checked_checkbox")! as UIImage
+        let uncheckedImage = UIImage(contentsOfFile: "unchecked_checkbox")! as UIImage
+        
+        var isChecked: Bool = false {
+            didSet {
+                if isChecked == true {
+                    self.setImage(checkedImage, for: UIControl.State.normal)
+                } else {
+                    self.setImage(uncheckedImage, for: UIControl.State.normal)
+
+                }
+            }
+        }
+        override func awakeFromNib() {
+            self.addTarget(self, action: #selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
+            self.isChecked = false
+        }
+        @objc func buttonClicked(sender: UIButton) {
+            if sender == self{
+                isChecked = !isChecked
+            }}
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
